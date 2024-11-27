e95462d273168b9f8044d0f42a8f5d911e0d66cc
Serdjd <sergiodobladomunoz@gmail.com>
2024-11-27 10:58:54 +0100

    UPD: examen terminado

---
+++ src/app/data.js
@@ -0,0 +1,41 @@
+export const imagenes = [
+    {
+        titulo: 'Trolleado',
+        url: 'https://y.yarn.co/4189348f-d197-4f36-bb21-52df260560b9_text.gif'
+    },
+    {
+        titulo: 'Chill Guy',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCLZDcPHs-5vKT4VUq5Pfx8lkXOzPB3-iwbw&s'
+    },
+    {
+        titulo: 'Esto es una hoja bastante chula',
+        url: 'https://www.educaciontrespuntocero.com/wp-content/uploads/2020/04/mejores-bancos-de-imagenes-gratis.jpg'
+    },
+    {
+        titulo: 'Esto vendría a ser un corazón bastante chulo',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmMjBnAwXPnJLWVOMIArsXd5tSGhpizxXA6g&s'
+    }
+]
+
+export const books = [
+    {
+        id: 0,
+        titulo: 'El Camino de los reyes',
+        editMode: false
+    },
+    {
+        id: 1,
+        titulo: 'Palabras Radiantes',
+        editMode: false
+    },
+    {
+        id: 2,
+        titulo: 'Juramentada',
+        editMode: false
+    },
+    {
+        id: 3,
+        titulo: 'Ritmos de guerra',
+        editMode: false
+    }
+]
---
+++ src/app/library.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { books } from "./data";
+
+export default function Library() {
+    const [libros, setLibros] = useState(books)
+    //Establece el modo edición en el libro específico
+    function edit(id) {
+        setLibros(
+            libros.map(
+                libro => {
+                    if(libro.id === id) {
+                        return {...libro, editMode: true}
+                    }
+                    else {
+                        return libro
+                    }
+                }
+            )
+        )
+    }
+    //Gudarda el núevo titulo y desactiva el modo edición
+    function save(id,title) {
+        setLibros(
+            libros.map(
+                libro => {
+                    if(libro.id === id) {
+                        return {...libro, titulo: title, editMode: false}
+                    }
+                    else {
+                        return libro
+                    }
+                }
+            )
+        )
+    }
+
+    return(
+        <>
+            <h2>Libros</h2>
+            {
+                libros.map(
+                    libro => <Book book={libro} edit={edit} save={save} key={libro.id}/>
+                )
+            }
+        </>
+        
+    )
+}
+
+function Book({book,edit,save}) {
+    const [libro,setLibro] = useState(book)
+    //Actualiza el estado conforme el usuario escribe
+    function handleChange(e) {
+        setLibro({...libro,titulo: e.target.value})
+    }
+    return (
+        <>
+            <li>
+                {
+                    book.editMode
+                    ? 
+                    <>
+                        <input type="text" value={libro.titulo} onChange={handleChange}></input>
+                        <button onClick={() => save(libro.id)}>Guardar</button>
+                    </>
+                    : 
+                    <>
+                        <label>{libro.titulo}</label>
+                        <button onClick={() => edit(libro.id,libro.titulo)}>Editar</button>
+                    </>
+                    
+                }
+            </li>
+        </>
+    )
+}
